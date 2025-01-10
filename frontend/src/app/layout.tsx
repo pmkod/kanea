0778f158831d1138b7f939393ec68b4ad683a69e
Kodossou <pierremariekod@gmail.com>
2025-01-10 14:51:45 +0000

    Add analytics to frontend

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -4,6 +4,8 @@ import { appName } from "@/constants/app-constants";
 import { Metadata } from "next";
 import BaseLayoutWrapper from "./base-layout-wrapper";
 import { headers } from "next/headers";
+import Script from "next/script";
+
 
 export const metadata: Metadata = {
   title: {
@@ -12,11 +14,15 @@ export const metadata: Metadata = {
   },
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const nonce = headers().get("x-nonce");
-  return <BaseLayoutWrapper>{children}</BaseLayoutWrapper>;
+	const nonce = (await headers()).get("x-nonce") || undefined;
+  return <BaseLayoutWrapper>{children}
+        <Script async
+					nonce={nonce}
+          src="https://analytics.kanea.net/script.js" data-website-id="db8a1067-555d-476a-ac0f-9809060e1de3" />
+  </BaseLayoutWrapper>;
 }
