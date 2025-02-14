367ef16caa904f6c138813b93e364947ae5b3e46
Kodossou <pierremariekod@gmail.com>
2025-02-14 21:55:00 +0000

    Fix

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -6,7 +6,6 @@ import BaseLayoutWrapper from "./base-layout-wrapper";
 import { headers } from "next/headers";
 import Script from "next/script";
 
-
 export const metadata: Metadata = {
   title: {
     default: appName,
@@ -19,10 +18,16 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-	const nonce = (await headers()).get("x-nonce") || undefined;
-  return <BaseLayoutWrapper>{children}
-        <Script async
-					nonce={nonce}
-          src="https://analytics.4ml3f81l4vtbdgwuldhdcqwbq7reg4oiffr3xdbi.kodossou.com/script.js" data-website-id="16882b32-b23d-4818-8acd-31daf86ec5cd" />
-  </BaseLayoutWrapper>;
+  const nonce = headers().get("x-nonce") || undefined;
+  return (
+    <BaseLayoutWrapper>
+      {children}
+      <Script
+        async
+        nonce={nonce}
+        src="https://analytics.4ml3f81l4vtbdgwuldhdcqwbq7reg4oiffr3xdbi.kodossou.com/script.js"
+        data-website-id="16882b32-b23d-4818-8acd-31daf86ec5cd"
+      />
+    </BaseLayoutWrapper>
+  );
 }
